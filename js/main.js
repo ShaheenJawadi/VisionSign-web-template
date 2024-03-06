b92e3e2c08c2b38884b5b9b212eff6ec0e04bcdd
shaheenj <jawadishaheen@gmail.com>
2024-03-06 16:46:42 +0100

    Created header component for the pages

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+    var $window = $(window);
+    $window.on('load', function (key, value) {
+
+        $(".header").load('../components/header.html')
+
+
+
+
+    });
+})(jQuery);
