4a9a4c1dcd77f9bfc61ceaf4b313ea3f9330403f
shaheenj <jawadishaheen@gmail.com>
2024-03-06 19:33:00 +0100

    Created newsLetter  component  for the main page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
         $('.intro_section1').load('../components/main_page/intro_section.html')
         $('.intro_section2').load('../components/main_page/intro_section2.html')
         $('.why_us').load('../components/main_page/why_us.html')
+        $('.newsletter').load('../components/main_page/newsletter.html')
 
     });
 })(jQuery);
