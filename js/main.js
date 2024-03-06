09d7b095180429ba796bb1ae2fe7cb6634258f04
shaheenj <jawadishaheen@gmail.com>
2024-03-06 17:21:35 +0100

    Created Banner  component for the main page

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@
     $window.on('load', function (key, value) {
 
         $(".header").load('../components/header.html')
-
-
-
-
+        $('.banner').load('../components/main_page/banner.html')
     });
 })(jQuery);
