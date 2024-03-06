d064b33371659e7cb928c4df86a4c2125c789be0
shaheenj <jawadishaheen@gmail.com>
2024-03-06 18:50:30 +0100

    Created whyUs  component  for the main page

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@
 
         $(".header").load('../components/header.html')
         $('.banner').load('../components/main_page/banner.html')
-        $('.intro_section').load('../components/main_page/intro_section.html')
+        $('.intro_section1').load('../components/main_page/intro_section.html')
         $('.intro_section2').load('../components/main_page/intro_section2.html')
+        $('.why_us').load('../components/main_page/why_us.html')
+
     });
 })(jQuery);
