84dff7f9f45fa4eba9cf6713db8b5f03c4c506c9
shaheenj <jawadishaheen@gmail.com>
2024-03-06 17:55:57 +0100

    Created Intro  component for the main page

--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@
 
         $(".header").load('../components/header.html')
         $('.banner').load('../components/main_page/banner.html')
+        $('.intro_section').load('../components/main_page/intro_section.html')
     });
 })(jQuery);
